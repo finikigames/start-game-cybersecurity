8fc5022e934a19d835c3f5d95deb8ac7db752497
Anton Sukharev <sanjaplushka@gmail.com>
2023-10-02 16:56:14 +0500

    Add continue config

---
+++ Assets/Scripts/Global/UI/ContinueView/ContinueData.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+namespace Global.UI.ContinueView {
+    [Serializable]
+    public class ContinueData : IWindowData {
+        public string Id;
+        public Color Color;
+    }
+}
