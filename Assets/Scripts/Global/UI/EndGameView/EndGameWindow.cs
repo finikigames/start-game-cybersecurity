7c9451e831be592b2d19f7eba528d00cf1342884
Victor Kondratenko <wionvictor@gmail.com>
2023-10-02 18:01:06 +0500

    End game window

---
+++ Assets/Scripts/Global/UI/EndGameView/EndGameWindow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Global.UI.EndGameView {
+    public class EndGameWindow : BaseWindow {
+        public Button Button;
+
+        public override void Initialize(string id) {
+            Button.onClick.RemoveAllListeners();
+            Button.onClick.AddListener(() => SceneManager.LoadScene("Menu"));
+        }
+    }
+}
